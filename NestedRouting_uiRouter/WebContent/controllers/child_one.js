da023a1eaf8498c523db12b8d6fc0e79f7001918
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2018-09-07 13:42:06 +0530

    push

---
+++ NestedRouting_uiRouter/WebContent/controllers/child_one.js
@@ -0,0 +1,3 @@
+app.controller("child_one",function($scope){
+	$scope.var_three="I am from child one as you excepted...... !  :P";
+});
---
+++ NestedRouting_uiRouter/WebContent/controllers/child_two.js
@@ -0,0 +1,3 @@
+app.controller("child_two",function($scope){
+	$scope.var_four="I am from child two as you excepted...... !  :P";
+});
